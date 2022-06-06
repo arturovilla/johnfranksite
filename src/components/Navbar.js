0caa1bad51348991d26764b8b83d6dba8f14c88f
arturovilla <acvturi@gmail.com>
2022-06-06 13:26:23 -0500

    adding navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="Navigationbar">
+      <div className="topL">Left</div>
+      <div className="topC">Center</div>
+      <div className="topR">Right</div>
+    </div>
+  );
+}
+
+export default Navbar;
